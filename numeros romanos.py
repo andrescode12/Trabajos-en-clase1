5f0029e40a3822ae5430553d2ae491938c1cf245
Jairo Andrés Gira Quisbert <126636732+andrescode12@users.noreply.github.com>
2023-05-29 21:12:26 -0400

    numero romanos

---
+++ numeros romanos.py
@@ -0,0 +1,20 @@
+def conversion_entero_romano(entero):
+    numeros=[1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    numerales=["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+
+    numeral=""
+    i=0
+
+    while entero>0:
+        for _ in range(entero//numeros[i]):
+            numeral +=numerales[i]
+            entero -=numeros[i]
+
+        i += 1
+    
+    return numeral
+
+num=int(input("ingre el valor que desea convertir "))
+numera=conversion_entero_romano(num)
+
+print("su numero {} en numero romanos es: {}".format(num,numera))
